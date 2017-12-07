ef811b943c32b4e00a8249d35a02f66f1f44fac4
Tom <thomas.keepings@method4.co.uk>
2017-12-07 17:25:26 +0000

    added an example unit test for school service

---
+++ Services.Tests/SchoolServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using Persistance;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestBuilder;
+
+namespace Services.Tests {
+    [TestClass]
+    public class SchoolServiceTest {
+
+        private static IPersistable<School> testSchoolPersistance;
+        private static ISchoolService service;
+
+        [ClassInitialize]
+        public static void Init(TestContext context) {
+            testSchoolPersistance = new TestSchoolPersistance();
+            service = new SchoolService(testSchoolPersistance);
+        }
+
+        [TestMethod]
+        public void GetTopPerformer() {
+            //Arrange
+            Student student;
+
+            //Act
+            student = service.GetTopStudent();
+
+            //Assert
+            Assert.AreEqual(student.Name, "Test Student", "The reported top performing student is incorrect");
+        }   
+    }
+}
---
+++ TestBuilder/TestSchoolPersistance.cs
@@ -0,0 +1,33 @@
+﻿using Models;
+using Persistance;
+using System;
+using System.Collections.Generic;
+
+namespace TestBuilder {
+    public class TestSchoolPersistance : IPersistable<School> {
+        private School school = new School() {
+            Name = "My Test School",
+            Students = new List<Student>() {
+                new Student() {
+                    Name = "Test Student",
+                    DateOfBirth = new DateTime(1,1,1)
+
+                }
+            }
+        };
+
+        public School Data {
+            get {
+                return school;
+            }
+
+            set {
+                school = value;
+            }
+        }
+
+        public int Save() {
+            return 1;
+        }
+    }
+}
